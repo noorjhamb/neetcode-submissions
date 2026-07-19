e7ba88247709be4cc3432fa461dd36452472aea3
noorjhamb <noorjhamb.bt24cse@pec.edu.in>
2026-07-19 08:00:10 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        /*unordered_map<char, int> state1;
+        unordered_map<char , int> state2;
+        for(char x :s){
+            state1[x]++;    }
+
+        for(char x :t){
+            state2[x]++;     }
+
+        return state1==state2;*/
+
+        unordered_map<char, int> freq;
+        for(char c : s){
+            freq[c]++;
+        }
+        for(char c: t){
+            if(--freq[c]<0)return false;
+
+        }
+        return true;
+
+        /*for(auto p : freq){
+            if(p.second!=0)return false;
+        }
+        return true ;*/
+
+
+        
+    }
+};
